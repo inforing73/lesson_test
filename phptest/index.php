e61d4dcf18eac568d2f880f9831c0f480e142296
lesson_test <awalarm@mail.ru>
2019-07-10 23:09:02 +0400

    Update index.php

--- phptest/index.php
+++ phptest/index.php
@@ -6,9 +6,10 @@
 
 <BR>
 <FORM method="POST" action="sumstr.php"> Введити тект который нужно проверить</br>
-<TEXTAREA name="countWords" cols="150" rows="25"> </TEXTAREA>
+<TEXTAREA name="countWords" cols="100" rows="20"> </TEXTAREA>
 
 <BR>
+  <p>тест гита</p>
 <BR>
 <INPUT type="submit" value="Проверить">
 </FORM>

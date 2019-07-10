85f04165a432fd80d8a770944e72eff633cdd0a5
Pasha Shalunov <awalarm@mail.ru>
2019-07-11 00:25:24 +0400

    test error

--- phptest/index.php
+++ phptest/index.php
@@ -3,14 +3,10 @@
 <TITLE> Уроки по PHP </title>
 </HEAD>
 <BODY>
-<p>test git</p>
+<p>test git2</p>
 <BR>
 <FORM method="POST" action="sumstr.php"> Введити тект который нужно проверить</br>
-<<<<<<< HEAD
 <TEXTAREA name="countWords" cols="60" rows="25"> </TEXTAREA>
-=======
-<TEXTAREA name="countWords" cols="100" rows="20"> </TEXTAREA>
->>>>>>> e61d4dcf18eac568d2f880f9831c0f480e142296
 
 <BR>
   <p>тест гита</p>

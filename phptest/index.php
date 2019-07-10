71c003d2642add9c229ef2cfd7bcbe5e8364f7d0
Pasha Shalunov <awalarm@mail.ru>
2019-07-10 23:07:09 +0400

    test git

--- phptest/index.php
+++ phptest/index.php
@@ -3,10 +3,10 @@
 <TITLE> Уроки по PHP </title>
 </HEAD>
 <BODY>
-
+<p>test git</p>
 <BR>
 <FORM method="POST" action="sumstr.php"> Введити тект который нужно проверить</br>
-<TEXTAREA name="countWords" cols="150" rows="25"> </TEXTAREA>
+<TEXTAREA name="countWords" cols="60" rows="25"> </TEXTAREA>
 
 <BR>
 <BR>

e775c4a3439934dd01f7aaed30f09c1e5cfeed0b
Shalunov Pasha <shalunov.pasha@gmail.com>
2019-07-17 22:32:27 +0400

    write to file CSV from form

--- phptest/index.php
+++ phptest/index.php
@@ -1,15 +1,13 @@
+<!DOCTYPE   html>
 <HTML>
 <HEAD>
-<TITLE> Уроки по PHP </title>
+    <TITLE> Уроки по PHP </title>
 </HEAD>
 <BODY>
-<p>test git2</p>
-<BR>
-<FORM method="POST" action="sumstr.php"> Введити тект который нужно проверить</br>
+
+<FORM method="POST" action="sumstr.php"> Введити текст который нужно проверить</br>
 <TEXTAREA name="countWords" cols="60" rows="25"> </TEXTAREA>
 
-<BR>
-  <p>тест гита</p>
 <BR>
 <INPUT type="submit" value="Проверить">
 </FORM>
--- phptest/sumstr.php
+++ phptest/sumstr.php
@@ -1,24 +1,38 @@
 
 <?php
 
+//Разбиваем текс на массив.
+$arr = preg_split("/[\s,=<>.!\"\'?()]+/", $_POST['countWords'], 0, PREG_SPLIT_NO_EMPTY);
 
 
-$arr = preg_split("/[\s,=<>.!\"\'?()0-9]+/", $_POST[countWords], 0, PREG_SPLIT_NO_EMPTY);
-$numberOfWords = count($arr);
+$headers = array("Слово", "Сколько раз"); //Заголовки для таблицы.
+$arr2 = array(array_count_values($arr)); // Подсчет повторений
+$arr3 = array_unique($arr); // Убираем повторяющие значния
+$fh = fopen('file.csv', 'w');
+fputcsv($fh, $headers, ";"); // Запись заоловка в таблицу
 
-function numberOfRepetitions (array $arr) {
-
-    foreach (array_count_values($arr) as $key => $value) {
-        $number = substr($value, -1);
-        $ending = 'раз';
-        if ($number > 1 && $number < 5)
-            $ending .= 'а';
-
-        echo "Слово \"$key\" встречается в тексте \"$value\" $ending.<br>",PHP_EOL;
+    foreach ($arr2 as $value2) {
+        $d = array_map(null, $arr3, $value2);
+        for ($n = 0; $n < count($arr3); $n++){
+        fputcsv($fh,  $d[$n],";");
+        }
     }
-}
 
-echo "Общее количество слов: {$numberOfWords}<br>";
-numberOfRepetitions($arr);
+$f = file_get_contents('file.csv');
+$f = iconv("UTF-8", "WINDOWS-1251", $f);
+file_put_contents('file.csv', $f);
+
 
 ?>
+
+
+<pre>
+    <?php
+
+    print_r($d);
+    print_r($value2);
+    print_r( array_unique($arr));
+
+    ?>
+</pre>
+
